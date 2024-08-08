83e651fdad0cdba5ecd558745143305626e2cca1
saifulnizam <saifultech.official@gmail.com>
2024-08-08 23:25:58 +0800

    fix issue

--- test5.py
+++ test5.py
@@ -1,9 +1,12 @@
-import nfc
-
-def list_devices():
-    with nfc.ContactlessFrontend('usb') as clf:
-        print("NFC reader initialized. Listing devices...")
-        print(clf.list_devices())
-
-if __name__ == "__main__":
-    list_devices()
+from nfc import ContactlessFrontend
+from time import sleep
+ 
+def connected(tag):
+    ident = ''.join('{:02x}'.format(ord(c)) for c in tag.identifier)
+    print(ident)
+    return False
+ 
+clf = ContactlessFrontend('usb')
+while True:
+    clf.connect(rdwr={'on-connect': connected})
+    sleep(1)
